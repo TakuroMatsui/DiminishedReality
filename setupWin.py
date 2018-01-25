002939d48f2f4752a4dd23b45ff76c604c19c3a6
TakuroMatsui <electronic_godzilla@yahoo.co.jp>
2018-01-25 13:19:31 +0900

    add setupWin

---
+++ setupWin.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("mkdir DAE\\data\\original")
+os.system("mkdir DAE\\model")
+os.system("mkdir DAE\\data\\dataset\\input DAE\\data\\dataset\\output DAE\\data\\dataset\\mask")
+os.system("mkdir DAE\\data\\testset\\input DAE\\data\\testset\\output DAE\\data\\testset\\mask")
+
+os.system("mkdir Detection\\data\\original\\input Detection\\data\\original\\output")
+os.system("mkdir Detection\\model")
+os.system("mkdir Detection\\data\\dataset\\input Detection\\data\\dataset\\output")
+os.system("mkdir Detection\\data\\testset\\input Detection\\data\\testset\\output")
